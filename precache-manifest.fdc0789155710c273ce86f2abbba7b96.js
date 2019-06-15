100da4a2a2f1c5b200ca7222ef4969e158a8701a
snaag <04_miffy@naver.com>
2019-06-16 04:14:48 +0900

    Updates

---
+++ precache-manifest.fdc0789155710c273ce86f2abbba7b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e3d9e20d2ad16d77a7b8d8c8fdf4e2",
+    "url": "/bnns-cra/index.html"
+  },
+  {
+    "revision": "1f703d367c34f9faaa26",
+    "url": "/bnns-cra/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0ffe313e7aba794956ed",
+    "url": "/bnns-cra/static/js/2.6e828243.chunk.js"
+  },
+  {
+    "revision": "1f703d367c34f9faaa26",
+    "url": "/bnns-cra/static/js/main.82bbceda.chunk.js"
+  },
+  {
+    "revision": "5f95b195d4b133f5963d",
+    "url": "/bnns-cra/static/js/runtime~main.75e29f15.js"
+  },
+  {
+    "revision": "2eea046d6162af5dbae13d83ee461207",
+    "url": "/bnns-cra/static/media/loading.2eea046d.gif"
+  }
+]);
